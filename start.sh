31d8cda4b6015daa03232a72acc487e2d8f9b8c5
Rodrigo Vilar <rodrigovilar@dcx.ufpb.br>
2017-07-02 02:26:22 -0300

    Stop

--- start.sh
+++ start.sh
@@ -1,53 +1,93 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo 'Usage: sh start.sh TASK_NUM TECHNOLOGY' 
-    exit 1
-fi
-
-if [[ $1 = *[[:digit:]]* ]]; then
-    if [[ $1 < 1 || $1 > 14 ]]; then
-        echo "TASK is $1 but should be between 1 and 14"
+CheckTaskRange() {
+    if [[ $1 = *[[:digit:]]* ]]; then
+        if [[ $1 < 1 || $1 > 14 ]]; then
+            echo "TASK is $1 but should be between 1 and 14"
+            exit 1
+        fi
+    else
+        echo "TASK $1 should be an integer"
         exit 1
     fi
-else
-    echo "TASK $1 should be an integer"
-    exit 1
-fi
+}
 
-if ! [ $2 == "ror" -o $2 == "mg" ]; then
-    echo 'TECHNOLOGY must be ror for Ruby on Rails or mg for Angular M'
-    echo 'Usage: sh start.sh TASK_NUM TECHNOLOGY'
-    exit 1
-fi
+CheckTechnology() {
+    if ! [ $1 == "ror" -o $1 == "mg" ]; then
+        echo 'TECHNOLOGY must be ror for Ruby on Rails or mg for Angular M'
+        echo 'Usage: sh start.sh TASK_NUM TECHNOLOGY'
+        exit 1
+    fi
+}
 
-GITHUB_ACCOUNT=`sh github.account.data`
+CheckParams() {
+    if [ "$1" -ne 2 ]; then
+        echo 'Usage: start.sh TASK_NUM TECHNOLOGY' 
+        exit 1
+    fi
 
-if [ -z $GITHUB_ACCOUNT ]; then
-    echo 'Run setup.sh before starting a task'
-    exit 1
-fi
+    CheckTaskRange $2
+    CheckTechnology $3
+}
 
-git clone https://github.com/$GITHUB_ACCOUNT/$2_tasks
+CheckFile() {
+    if [ -f "$1" ]; then
+        echo "File $1 found."
+    else
+        echo "File $1 not found."
+        exit 1
+    fi
+}
 
-if [ $? -eq 0 ]; then
-    echo "Repository https://github.com/$GITHUB_ACCOUNT/$2_tasks cloned successfully"
-else
-    echo "Repository https://github.com/$GITHUB_ACCOUNT/$2_tasks could not be cloned"
-    exit 1
-fi
+StoreData() {
+    echo "echo $1" | xargs > $2
+}
 
-cd $2_tasks
+CloneRepository() {
+    GITHUB_ACCOUNT=`sh github.account.data`
 
-git reset --hard T$1
+    if [ -z $GITHUB_ACCOUNT ]; then
+        echo 'Run setup.sh before starting a task'
+        exit 1
+    fi
 
-if [ $? -eq 0 ]; then
-    echo "Code moved to Task $1 successfully"
-else
-    echo "Code could not be moved to Task $1"
-    exit 1
-fi
+    git clone https://github.com/$GITHUB_ACCOUNT/$1
 
-git checkout -b A$1
+    if [ $? -eq 0 ]; then
+        echo "Repository https://github.com/$GITHUB_ACCOUNT/$1 cloned successfully"
+    else
+        echo "Repository https://github.com/$GITHUB_ACCOUNT/$1 could not be cloned"
+        exit 1
+    fi
 
-echo `date +%s` > begin.data
+    cd $1
+}
+
+HardResetToTag() {
+    git reset --hard $1
+
+    if [ $? -eq 0 ]; then
+        echo "Code moved to Task $1 successfully"
+    else
+        echo "Code could not be moved to Task $1"
+        exit 1
+    fi
+}
+
+NewBranch() {
+    git checkout -b $1
+}
+
+SaveBegin() {
+    NOW=`date +%s`
+    echo "$NOW" > begin.data
+}
+
+CheckParams $# $1 $2
+CheckFile github.account.data
+StoreData $1 task.data
+StoreData $2 technology.data
+CloneRepository $2_tasks
+HardResetToTag T$1
+NewBranch A$1
+SaveBegin
---
+++ stop.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+CheckParams() {
+    if [ "$#" -ne 0 ]; then
+        echo 'Usage: stop.sh' 
+        exit 1
+    fi
+}
+
+CheckFile() {
+   if [ -f "$1" ] then;
+        echo "File $1 found."
+    else
+        echo "File $1 not found."
+        exit 1
+    fi
+}
+
+CheckContent() {
+    if [ -z $1 ]; then
+        echo 'Run start.sh before stoping a task'
+        exit 1
+    fi
+}
+
+CheckTask() {
+    CheckContent $1
+
+    if [[ $1 = *[[:digit:]]* ]]; then
+        if [[ $1 < 1 || $1 > 14 ]]; then
+            echo "TASK is $1 but should be between 1 and 14"
+            exit 1
+        fi
+    else
+        echo "TASK $1 should be an integer"
+        exit 1
+    fi
+}
+
+CheckTechnology() {
+    if ! [ $1 == "ror" -o $1 == "mg" ]; then
+        echo 'TECHNOLOGY must be ror for Ruby on Rails or mg for Angular M'
+        echo 'Usage: sh start.sh TASK_NUM TECHNOLOGY'
+        exit 1
+    fi
+}
+
+StartServer() {
+    PID=0
+
+    if [ $1 == "ror" ]; then
+        rails server &
+        PID=$!
+    else
+        ng serve --port 3000 &
+        PID=$!
+    fi
+
+    return PID
+}
+
+RunTests() {
+    SELENIUM_BROWSER=chrome node $3
+    TEST_RESULT=$?
+
+    kill -9 $2
+
+    if [ $TEST_RESULT -eq 0 ]; then
+        echo "Tests passed for Task $1"
+    else
+        echo "Tests failed for Task $1. Please fix them and try again."
+        exit 1
+    fi
+}
+
+SaveEnd() {
+    NOW=`date +%s`
+    echo "$NOW" > end.data
+}
+
+SendFilesToGithub() {
+    git add .
+    if [ $? -ne 0 ]; then
+        echo "Could not add files to git" 
+        exit 1
+    fi
+
+    git commit -m " Task $1 finished"
+    if [ $? -ne 0 ]; then
+        echo "Could not commit files on git" 
+        exit 1
+    fi
+
+    git push origin A$1
+    if [ $? -ne 0 ]; then
+        echo "Could not push files to Github" 
+        exit 1
+    fi
+}
+
+CleanUp() {
+    cd
+    rm -rf ~/${1}_tasks
+    rm github.account.data technology.data task.data
+}
+
+CheckParams
+CheckFile task.data
+TASK=`sh task.data`
+CheckTask $TASK
+CheckFile technology.data
+TECHNOLOGY=`sh technology.data`
+CheckTechnology $TECHNOLOGY
+CheckFile github.account.data
+GITHUB_ACCOUNT=`sh github.account.data`
+StartServer $TECHNOLOGY
+SERVER_PID=$?
+RunTests $TASK $SERVER_PID task-e2e.js
+SaveEnd
+SendFilesToGithub $TASK
+CleanUp $TECHNOLOGY
